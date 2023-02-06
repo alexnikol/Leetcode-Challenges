59dc7024b8e27c24b8857728b1e96a2a4d81ea54
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-02-06 15:44:09 +0200

    Added all tests cases from LC

--- Leetcode ChallengesTests/Easy/1. Two Sum/TwoSumTests.swift
+++ Leetcode ChallengesTests/Easy/1. Two Sum/TwoSumTests.swift
@@ -15,6 +15,12 @@ final class TwoSumTests: XCTestCase {
         
         let result1 = sut.twoSum([2, 7, 11, 15], 9)
         XCTAssertEqual(result1, [0, 1])
+        
+        let result2 = sut.twoSum([3, 2, 4], 6)
+        XCTAssertEqual(result2, [1, 2])
+        
+        let result3 = sut.twoSum([3, 3], 6)
+        XCTAssertEqual(result3, [0, 1])
     }
     
     // MARK: - Helpers
