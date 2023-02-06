a5286308cc5f0a18b66324211d78328f26ee03d2
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-02-06 15:42:18 +0200

    Solved for case when answer contain first value from array

--- Leetcode ChallengesTests/Easy/1. Two Sum/TwoSumTests.swift
+++ Leetcode ChallengesTests/Easy/1. Two Sum/TwoSumTests.swift
@@ -11,6 +11,10 @@ import XCTest
 final class TwoSumTests: XCTestCase {
     
     func test_sut() {
+        let sut = makeSUT()
+        
+        let result1 = sut.twoSum([2, 7, 11, 15], 9)
+        XCTAssertEqual(result1, [0, 1])
     }
     
     // MARK: - Helpers
@@ -21,7 +25,15 @@ final class TwoSumTests: XCTestCase {
     
     class Solution {
         func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-            return []
+            var result: [Int] = []
+            for index in 0..<nums.count {
+                if nums[index] + nums[index + 1] == target {
+                    result.append(index)
+                    result.append(index + 1)
+                    break
+                }
+            }
+            return result
         }
     }
 }
