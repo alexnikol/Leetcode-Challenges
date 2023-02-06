f7def5e66553fa4cfe1f135939955f33d87ac3c4
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-02-06 15:34:15 +0200

    Setup

---
+++ Leetcode ChallengesTests/Easy/1. Two Sum/TwoSumTests.swift
@@ -0,0 +1,27 @@
+// Copyright © 2023 Almost Engineer. All rights reserved.
+
+import XCTest
+
+// 1. Two Sum
+/// Given an array of integers nums and an integer target, return indices of the two numbers such
+/// that they add up to target.
+/// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+/// You can return the answer in any order.
+
+final class TwoSumTests: XCTestCase {
+    
+    func test_sut() {
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> Solution {
+        return Solution()
+    }
+    
+    class Solution {
+        func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+            return []
+        }
+    }
+}
