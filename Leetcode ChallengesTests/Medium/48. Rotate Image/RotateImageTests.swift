03395a80861050b1172d41fa90cd82d9c6aa36fc
Oleksandr Nikolaichuk <alexksnikol@gmail.com>
2023-02-08 14:03:04 +0200

    Fixed warning

--- Leetcode ChallengesTests/Medium/48. Rotate Image/RotateImageTests.swift
+++ Leetcode ChallengesTests/Medium/48. Rotate Image/RotateImageTests.swift
@@ -6,6 +6,7 @@ import XCTest
 /// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 /// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
+#warning("Rework this in more effecient way")
 final class RotateImageTests: XCTestCase {
     
     func test_sut() {
